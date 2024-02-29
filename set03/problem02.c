fee23c4aefe6c12a16960419eb1cdf0766d5036d
namanajahnavi <namanaj.bsc23@rvu.edu.in>
2024-02-29 22:36:09 +0530

    test

--- set03/problem02.c
+++ set03/problem02.c
@@ -35,6 +35,6 @@ void output(float x1, float y1, float x2, float y2,float x3, float y3, int resul
 int main() {
     float x1, y1, x2, y2, x3, y3;
     input_triangle(&x1, &y1, &x2, &y2, &x3, &y3);
-    output(x1, y1, x2, y2, x3, y3);
+    output(x1, y1, x2, y2, x3, y3, result);
     return 0;
 }
